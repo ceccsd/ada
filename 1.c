482c84768d8765303bb08f666e0c35f61aeefc75
ceccsd <usa300372@gmail.com>
2026-07-18 22:45:16 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,60 +1,78 @@
-#include<stdio.h>
-#define INFI 99
+#include <stdio.h>
 #define MAX 10
-int a[MAX][MAX],b[MAX][MAX],n,cost=0;
-void findmin(int * v1,int *v2) //finding the edge having minimum weight.
-{
-int edge=INFI,i,j;
-for(i=1;i<=n;i++)
-for(j=i+1;j<=n;j++)
-if(a[i][j]>0 && a[i][j]<edge)
-{
-edge=a[i][j];
-*v1=i;
-*v2=j;
+#define INF 999
+
+void kruskals(int n, int cost[MAX][MAX]) {
+    int ne = 1, mincost = 0;
+    int u, v, a, b, min;
+    int parent[MAX];
+
+    // Initialize parent array
+    for (int i = 0; i < MAX; i++) {
+        parent[i] = 0;
+    }
+
+    printf("\n\n********* Edges in the Minimum Spanning Tree *********\n");
+
+
+    while (ne < n) {
+        min = INF;
+        a = b = -1;
+
+        // Find the minimum cost edge (excluding 0-cost edges)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (cost[i][j] != 0 && cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+
+        // Find the root of both vertices
+        while (parent[u])
+            u = parent[u];
+        while (parent[v])
+            v = parent[v];
+        printf("%d, %d", u,v);
+
+        // If they belong to different sets, include this edge in MST
+        if (u != v) {
+            printf("%d> Edge (%d, %d) with cost %d\n", ne++, a, b, min);
+            mincost += min;
+            parent[v] = u; // Union operation directly in loop
+        }
+
+        // Mark the edge as used
+        cost[a][b] = cost[b][a] = INF;
+    }
+
+    printf("\nThe minimum spanning tree cost is: %d\n", mincost);
 }
+
+int main() {
+    int n, cost[MAX][MAX];
+
+    printf("\n\n********* KRUSKAL'S ALGORITHM *********\n\n");
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    printf("\nEnter the cost matrix:\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            scanf("%d", &cost[i][j]);
+        }
+    }
+
+    printf("\nEntered cost matrix:\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            printf("%d\t", cost[i][j]);
+        }
+        printf("\n");
+    }
+
+    kruskals(n, cost);
+    return 0;
 }
-void update(int root[],int v1,int v2)
-{
-int temp,i;
-temp=root[v2];
-for(i=1;i<=n;i++)
-{
-if(root[i]==temp)
-root[i]=root[v1];
-}
-}
-void kruskal()
-{
-int i ,v1,v2,root[MAX],edge,count=0;
-for(i=1;i<=n;i++)
-root[i]=i;
-i=0;
-while(i!=n-1)
-{
-findmin(&v1,&v2);
-edge=a[v1][v2];
-a[v1][v2]=a[v2][v1]=0; //do not select the same edge on next time.
-if(root[v1]!=root[v2])
-{
-printf("(%d,%d)\n",v1,v2);
-update(root,v1,v2);
-cost+=edge;
-i++;
-}
-}
-}
-int main()
-{
-int i,j;
-printf("\n Enter the number of vertices : ");
-scanf("%d",&n);
-printf("\n Enter the weighted graph : \n");
-for(i=1;i<=n;i++)
-for(j=1;j<=n;j++)
-scanf("%d",&a[i][j]);
-printf("\n Edges of spanning tree are:\n");
-kruskal();
-printf("\n Minimum cost=%d:",cost);
-return(0);
- }
